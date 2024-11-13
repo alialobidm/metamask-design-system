f6383b1f22df722068131b083466235d27196e5b
George Marshall <georgewrmarshall@gmail.com>
2024-11-13 15:50:57 -0800

    chore: removing unneeded files and folders that will be replaced (#105)
    ## **Description**
    
    This PR resolves [Issue
    #104](https://github.com/MetaMask/metamask-design-system/issues/104) by
    removing the unneeded folders and files as part of the design tokens
    migration process. Following the [Package Migration Process
    Guide](https://github.com/MetaMask/metamask-design-system/blob/main/docs/package-migration-process-guide.md#pr8-4-remove-files-and-directories-that-will-be-replaced-by-files-in-the-monorepo-root-directory)
    
    ## **Related issues**
    
    Fixes: https://github.com/MetaMask/metamask-design-system/issues/104
    
    ## **Manual testing steps**
    
    1. Verify that the files and folder mentioned in [this
    step](https://github.com/MetaMask/metamask-design-system/blob/main/docs/package-migration-process-guide.md#pr8-4-remove-files-and-directories-that-will-be-replaced-by-files-in-the-monorepo-root-directory)
    have been removed
    2. Verify any other files and folders like storybook have been removed.
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs)
    - [x] I've completed the PR template to the best of my ability.
    - [x] I’ve included tests if applicable.
    - [x] I’ve documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable.
    - [x] I’ve applied the appropriate labels to the PR.
    
    ## **Pre-merge reviewer checklist**
    
    - [ ] I've manually tested the PR (e.g., pulled and built the branch,
    checked the app, and verified the updated tokens).
    - [ ] I confirm that this PR addresses all acceptance criteria described
    in issue #104 and includes necessary testing evidence such as recordings
    or screenshots.

--- merged-packages/design-tokens/vite.config.mjs
+++
@@ -1,7 +0,0 @@
-// vite.config.js
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [react()], // Enables fast refresh for React during development
-});
