92545e80b590d1e765ed83b402bab7ea65a28960
George Marshall <georgewrmarshall@gmail.com>
2025-04-29 21:09:47 +0930

    chore: upgrading storybook and fixing theme (#608)
    ## **Description**
    
    This PR focuses on upgrading our Storybook implementation and removing
    the old logo in preparation for release and adding accessibility testing
    capabilities.
    
    Key improvements include:
    1. Upgrading Storybook and related addons to version 8.6.12
    2. Removing unnecessary theme configuration from the Storybook manager
    3. Removing old assets (logo.svg)
    4. Updating esbuild packages to version 0.25.3
    
    ## **Related issues**
    
    Fixes: N/A
    Related: https://github.com/MetaMask/metamask-design-system/pull/609
    
    ## **Manual testing steps**
    
    1. Pull the branch and install dependencies
    2. Start Storybook using `yarn storybook`
    3. Verify that all components render correctly in the updated Storybook
    environment
    
    ## **Screenshots/Recordings**
    
    ### **Before**
    Storybook still shows old logo
    
    
    https://github.com/user-attachments/assets/4ae09340-26cb-4799-aa37-eef032eb56c9
    
    ### **After**
    
    Not really any notable changes, just removing old logo and reverting
    back to some storybook defaults and ensuring stories work as expected.
    
    
    https://github.com/user-attachments/assets/298e6db6-a3cf-4e86-890c-70b8bb7a64d2
    
    ## **Pre-merge author checklist**
    
    - [x] I've followed [MetaMask Contributor
    Docs](https://github.com/MetaMask/contributor-docs)
    - [x] I've completed the PR template to the best of my ability
    - [x] I've included tests if applicable
    - [x] I've documented my code using [JSDoc](https://jsdoc.app/) format
    if applicable
    - [ ] I've applied the right labels on the PR (see [labeling
    guidelines](https://github.com/MetaMask/metamask-extension/blob/develop/.github/guidelines/LABELING_GUIDELINES.md)).
    Not required for external contributors.

--- apps/storybook-react/.storybook/manager.ts
+++ apps/storybook-react/.storybook/manager.ts
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/manager-api';
-import MetaMaskTheme from './theme';
+import { themes } from '@storybook/theming';
 
 addons.setConfig({
-  theme: MetaMaskTheme,
+  theme: themes.light,
 });
--- apps/storybook-react/.storybook/theme.ts
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-  base: 'light',
-  brandTitle: 'MetaMask Design System React UI',
-  brandImage: '../stories/assets/logo.svg',
-  brandTarget: '_blank',
-});
